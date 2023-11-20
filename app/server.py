475cc63556ea280f3b875377d818005d67984cd5
Magnus <magnus@bonusfamilien.dk>
2023-11-20 12:16:39 +0100

    fixed cfg lookup

--- app/server.py
+++ app/server.py
@@ -4,10 +4,11 @@ from dotenv import dotenv_values
 from healthservice import SQLHealthDB, HealthService
 import os
 
+cfg = dict()
 if os.path.exists(".env"):
     cfg = dotenv_values(".env")
 
-cfg["DB_CONN"] = os.environ.get("DB_CONN", cfg["DB_CONN"])
+cfg["DB_CONN"] = os.environ.get("DB_CONN", cfg.get("DB_CONN", None))
 
 app = FastAPI()
 db = SQLHealthDB(cfg)
